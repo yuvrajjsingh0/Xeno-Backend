859805398f9e9062a4a27c023a70adb87126fa50
Yuvraj Singh <yuvrajjsingh0@gmail.com>
2024-06-12 06:35:51 +0530

    Gen dummy data script

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import dbInit from './core/db/init';
 require('dotenv').config();
 
 import app from './api/app';
+import generateDummyData from './core/db/dummyData';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   dbInit();
-  //generateDummyData();
+  generateDummyData();
   startConsumer().catch(err => console.log(err));
 });
